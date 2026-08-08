db9592d204faed6890f1c70449cf2de31596aac2
Darshan D. Prabhu <77969007+Darshan0902@users.noreply.github.com>
2026-08-08 21:24:19 +0530

    Time: 15 ms (18.66%), Space: 21.4 MB (18.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        elements={}
+        elements = {}
         for num in nums:
             if num in elements:
                 elements[num] += 1
             else:
                 elements[num] = 1
-        return max(elements,key =elements.get)
-            
+        return max(elements,key = elements.get)
